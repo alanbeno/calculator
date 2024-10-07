cce37d361d29a6f9e1e90ff6c0957ff6d8b24092
NIRMAL RAVEENDRAN <alanbenoabraham@gmail.com>
2024-10-07 22:47:38 +0530

    counter

--- calculator/index.js
+++ calculator/index.js
@@ -1,8 +1,56 @@
 
 
-let username;
-document.getElementById('mybutton').onclick = function(){
-    username = document.getElementById('myinput').value;
-    document.getElementById("myid").textContent = `Hello ${username}`;
+// let username;
+// // 1).window.prompt
+// let age = window.prompt("enter your age");
 
+// //for alert box 
+// window.alert(`your age is ${age}`);
+
+
+// 2).using input
+// document.getElementById('mybutton').onclick = function(){
+//     username = document.getElementById('myinput').value;
+//     document.getElementById("myid").textContent = `Hello ${username}`;
+// }
+
+// 3).type conversion
+// let num = window.prompt("what is your age");
+// num = Number(num);
+// // while type conversion same goes for Boolean, String
+// num += 1;
+// console.log(num);
+// // output will be 251 since it will be accepting strings
+
+// COUNTER
+
+const increase = document.getElementById("incbtn");
+const decrease = document.getElementById("decbtn");
+const reset = document.getElementById("rebtn");
+const display = document.getElementById("myid");
+
+let counter = 0;
+
+increase.onclick = function(){
+    counter++;
+    display.textContent = counter;
 }
+
+decrease.onclick = function(){
+    counter--;
+    display.textContent = counter;
+}
+
+reset.onclick = function(){
+    counter = 0;
+    display.textContent = counter;
+}
+
+
+
+
+
+
+
+
+
